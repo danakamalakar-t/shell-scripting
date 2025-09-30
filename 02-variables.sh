6a3a8f45fec485a05631dd43b482dd13f3ab169b
danakamalakar-t <danakamalakar.t@gmail.com>
2025-10-01 00:14:56 +0530

    some conversations

---
+++ 02-variables.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1="KAMAL"
+PERSON2="RUNA"
+
+echo "$PERSON1::What is your name?"
+echo "$PERSON2::My name is $PERSON2. What is your name?"
+echo "$PERSON1::My name is $PERSON1. Nice to meet you"
+echo "$PERSON2::Nice to meet you too."
