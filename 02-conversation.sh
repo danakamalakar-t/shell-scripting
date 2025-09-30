d8e521c4c7fa48c765a41cc9723748be75b4b954
danakamalakar-t <danakamalakar.t@gmail.com>
2025-10-01 00:09:42 +0530

    some conversations

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Tweety::What is your name?"
+echo "Fruity::My name is Fruity. What is your name?"
+echo "Tweety::My name is Tweety. Nice to meet you"
+echo "Fruity::Nice to meet you too."
