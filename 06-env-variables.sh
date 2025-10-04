48974ed42c31ff9dfd5c1d8ed4cacb96f00a1ab6
danakamalakar-t <danakamalakar.t@gmail.com>
2025-10-04 07:18:05 +0530

    Variables2

---
+++ 06-env-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Course name is: $COURSE_NAME"
+echo "Student name is: $STUDENT_NAME"
+# Usage:
+# COURSE_NAME="Bash Scripting" STUDENT_NAME="Alice" ./06-env-variables.sh
+# Output:
+# Course name is: Bash Scripting
+# Student name is: Alice
