fb74c62bc94c8bd98abe44a6c4c2c09b36ac352e
danakamalakar-t <danakamalakar.t@gmail.com>
2025-10-04 07:15:03 +0530

    Variables2

---
+++ 05-variables2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Please enter your PIN code:"
+read -s PIN_CODE  # -s option to hide input
+echo "PIN code entered is: $PIN_CODE"
+# Usage: ./05-variables2.sh
+# Output:
+# Please enter your PIN code:
+# [Input is hidden]
+# PIN code entered is: 1234
