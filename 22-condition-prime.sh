e6dd8e32770573c0aea17fda97b07ca2d35f7afa
danakamalakar-t <danakamalakar.t@gmail.com>
2026-02-01 19:29:37 +0530

    even or odd

---
+++ 22-condition-prime.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Enter a number:"
+read number
+
+# check if the number is prime
+
+if [ $number -lt 2 ]; then
+    echo "$number is not prime."
+else
+    is_prime=true
+    for ((i=2; i*i<=number; i++)); do
+        if [ $(($number % $i)) -eq 0 ]; then
+            is_prime=false
+            break
+        fi
+    done
+
+    if [ "$is_prime" = true ]; then
+        echo "$number is prime."
+    else
+        echo "$number is not prime."
+    fi
+fi
