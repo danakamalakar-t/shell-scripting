2bbbe9ab2d68a2ed7bb39429de2baa57204e7b8e
danakamalakar-t <danakamalakar.t@gmail.com>
2026-02-03 10:30:52 +0530

    Installing servers

---
+++ 24-installation-using-functions.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ "$(id -u)" -ne 0 ]; then    
+    echo "Please run as root or use sudo"
+    exit 1 # Exit if not root
+fi
+
+# Install MySQL
+echo "Installing MySQL..."
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+        echo "$2 installation failed!"
+        exit 1
+    fi
+    echo "$2 installed successfully."
+}
+echo "Installing MySQL..."
+dnf install mysql -y
+VALIDATE $? "MySQL"
+
+echo "Installing Nginx..."
+dnf install nginx -y
+VALIDATE $? "Nginx"
+
+echo "Installing Python3..."
+dnf install python3 -y
+VALIDATE $? "Python3"
+
+# usage: ./24-installation-using-functions.sh
+# Output:
